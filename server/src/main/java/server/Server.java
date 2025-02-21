3af30d8b76de3c5d3241b7683669fc7075687833
guymt61 <jacob.leetham@gmail.com>
2025-02-20 20:22:11 -0700

    Finished initial implementation of register endpoint (positive case only)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,19 @@
 package server;
 
+import com.google.gson.Gson;
+import requestsresults.*;
 import spark.*;
+import service.*;
+import dataaccess.*;
+
 
 public class Server {
+    private final UserService userService;
+
+    public Server() {
+        UserDAO userDAO = new MemoryUserDAO();
+        userService = new UserService(userDAO);
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -10,12 +21,19 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::register);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object register(Request req, Response res) {
+        RegisterRequest regReq = new Gson().fromJson(req.body(), RegisterRequest.class);
+        RegisterResult regRes = userService.register(regReq);
+        return new Gson().toJson(regRes);
     }
 
     public void stop() {
