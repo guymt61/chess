179496caf5584395d9ec346c163ff0b4e0a49cb3
guymt61 <jacob.leetham@gmail.com>
2025-02-24 10:27:02 -0700

    Completed unit tests for register method in UserServiceTest.java

--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -5,7 +5,6 @@ import dataaccess.UserDAO;
 import exception.ResponseException;
 import model.AuthData;
 import model.UserData;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,21 +17,14 @@ class UserServiceTest {
     private UserDAO userDAO;
     private AuthDAO authDAO;
     private UserService service;
-    private UserData testUser1;
-    private UserData testUser2;
-    private UserData testUser3;
-    private UserData testUser4;
-    private UserData testUser5;
+    private final UserData testUser1 = new UserData("testUser1", "superSecure", "test@test.test");
+    private final UserData testUser2 = new UserData("testUser2", "superSecure", "test@test.test");
+    private final UserData testUser3 = new UserData("testUser3", "superSecure", "test@test.test");
+    private final UserData testUser4 = new UserData("testUser4", "superSecure", "test@test.test");
+    private final UserData testUser5 = new UserData("testUser5", "superSecure", "test@test.test");
 
     private RegisterRequest RegReqFromUserData(UserData data) {
         return new RegisterRequest(data.username(), data.password(), data.email());
-    }
-
-    private void CheckForUser1(RegisterResult result) {
-        //Asserts that the result has the right username and an authToken and that "testUser1" is in the userDAO
-        assertNotNull(userDAO.getUser("testUser1"));
-        assertEquals("testUser1", result.username());
-        assertNotNull(result.authToken());
     }
 
     private void CheckForAuth(String authToken, String username){
@@ -40,15 +32,6 @@ class UserServiceTest {
         AuthData authData = authDAO.getAuth(authToken);
         assertNotNull(authData);
         assertEquals(username, authData.user());
-    }
-
-    @BeforeAll
-    static void makeUsers() {
-        UserData testUser1 = new UserData("testUser1", "superSecure", "test@test.test");
-        UserData testUser2 = new UserData("testUser2", "superSecure", "test@test.test");
-        UserData testUser3 = new UserData("testUser3", "superSecure", "test@test.test");
-        UserData testUser4 = new UserData("testUser4", "superSecure", "test@test.test");
-        UserData testUser5 = new UserData("testUser5", "superSecure", "test@test.test");
     }
 
     @BeforeEach
@@ -62,7 +45,8 @@ class UserServiceTest {
     @DisplayName("Successful Registration - Empty DAO")
     void successRegister() throws ResponseException {
         RegisterResult result = service.register(RegReqFromUserData(testUser1));
-        CheckForUser1(result);
+        assertNotNull(userDAO.getUser("testUser1"));
+        CheckForAuth(result.authToken(), "testUser1");
     }
 
     @Test
@@ -73,18 +57,65 @@ class UserServiceTest {
         userDAO.createUser(testUser4);
         userDAO.createUser(testUser5);
         RegisterResult result = service.register(RegReqFromUserData(testUser1));
-        CheckForUser1(result);
+        assertNotNull(userDAO.getUser("testUser1"));
+        CheckForAuth(result.authToken(),"testUser1");
+    }
+
+    @Test
+    @DisplayName("Triple Successful Registration")
+    void tripleRegister() throws ResponseException {
+        RegisterResult result1 = service.register(RegReqFromUserData(testUser1));
+        RegisterResult result2 = service.register(RegReqFromUserData(testUser2));
+        RegisterResult result3 = service.register(RegReqFromUserData(testUser3));
+        assertNotNull(userDAO.getUser("testUser1"));
+        assertNotNull(userDAO.getUser("testUser2"));
+        assertNotNull(userDAO.getUser("testUser3"));
+        CheckForAuth(result1.authToken(), "testUser1");
+        CheckForAuth(result2.authToken(), "testUser2");
+        CheckForAuth(result3.authToken(), "testUser3");
     }
 
     @Test
     @DisplayName("Register Username Already In Use")
-    void registerTaken() {
+    void registerTaken(){
+        userDAO.createUser(testUser1);
+        try {
+            service.register(RegReqFromUserData(testUser1));
+            fail("Register did not throw an error");
+        } catch (ResponseException e) {
+            //ResponseException thrown as intended
+            assertEquals(403, e.StatusCode());
+        }
 
     }
 
     @Test
     @DisplayName("Incomplete Registration")
     void registerIncomplete() {
+        //Missing username
+        try {
+            service.register(new RegisterRequest(null, "pass", "email"));
+            fail("Register did not throw an error");
+        } catch (ResponseException e) {
+            //Exception thrown as intended
+            assertEquals(400, e.StatusCode());
+        }
+        //Missing password
+        try {
+            service.register(new RegisterRequest("user", null, "email"));
+            fail("Register did not throw an error");
+        } catch (ResponseException e) {
+            //Exception thrown as intended
+            assertEquals(400, e.StatusCode());
+        }
+        //Missing email
+        try {
+            service.register(new RegisterRequest("user", "pass", null));
+            fail("Register did not throw an error");
+        } catch (ResponseException e) {
+            //Exception thrown as intended
+            assertEquals(400, e.StatusCode());
+        }
 
     }
 
