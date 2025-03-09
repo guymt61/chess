bf864fea39f5d1364446e5d124db753eee5bbe1d
guymt61 <jacob.leetham@gmail.com>
2025-03-09 00:39:17 -0700

    Got temporary database changes to work in testing. Implemented passing tests for createUser

--- server/src/test/java/dataaccess/SQLUserDAOTest.java
+++ server/src/test/java/dataaccess/SQLUserDAOTest.java
@@ -7,28 +7,83 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SQLUserDAOTest {
 
-    private UserDAO userDAO;
+    private static UserDAO userDAO;
     private final UserData newUser1 = new UserData("testUser1", "superSecure", "test@test.test");
     private final UserData newUser2 = new UserData("testUser2", "superSecure", "test@test.test");
     private final UserData newUser3 = new UserData("testUser3", "superSecure", "test@test.test");
+
+    @BeforeAll
+    static void dupeTable() {
+        try {
+            userDAO = new MySQLUserDAO();
+        }
+        catch (Exception e) {
+            System.out.println(String.format("Test Setup Failed on account of %n", e));
+        }
+        var statement = "CREATE TABLE usersCopy LIKE users";
+        try (var conn = DatabaseManager.getConnection()) {
+            var cloner = conn.prepareStatement(statement);
+            cloner.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        statement = "INSERT INTO usersCopy SELECT * FROM users";
+        try (var conn = DatabaseManager.getConnection()) {
+            var inserter = conn.prepareStatement(statement);
+            inserter.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @AfterAll
+    static void replaceTable() {
+        var statement = "DROP TABLE users";
+        try (var conn = DatabaseManager.getConnection()) {
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        statement = "CREATE TABLE users LIKE usersCopy";
+        try (var conn = DatabaseManager.getConnection()) {
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        statement = "INSERT INTO users SELECT * FROM usersCopy";
+        try (var conn = DatabaseManager.getConnection()) {
+            var inserter = conn.prepareStatement(statement);
+            inserter.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        statement = "DROP TABLE usersCopy";
+        try (var conn = DatabaseManager.getConnection()) {
+            var preparedStatement = conn.prepareStatement(statement);
+            preparedStatement.execute();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     @Order(1)
     @DisplayName("Successful Create")
     void createUser() {
-        startTransaction();
         assertDoesNotThrow(() -> userDAO.createUser(newUser1));
-        rollback();
     }
 
     @Test
     @Order(2)
     @DisplayName("Username in Use")
     void createDupeUser() {
-        startTransaction();
-        userDAO.createUser(newUser1);
-        userDAO.createUser(newUser1);
-        rollback();
+        userDAO.createUser(newUser2);
+        int startingRowsCount = countRows();
+        userDAO.createUser(newUser2);
+        int currentRowsCount = countRows();
+        assertEquals(startingRowsCount,currentRowsCount, "Number of rows changed after adding existing user");
 
     }
 
@@ -41,30 +96,21 @@ class SQLUserDAOTest {
     @Test
     @Order(5)
     void clear() {
-        startTransaction();
-        userDAO.clear();
-        rollback();
     }
 
-    void startTransaction() {
-        var statement = "START TRANSACTION";
+    private int countRows() {
         try (var conn = DatabaseManager.getConnection()) {
-            userDAO = new MySQLUserDAO();
-            var startStatement = conn.prepareStatement(statement);
-            startStatement.execute();
-        }
-        catch (Exception e) {
-            System.out.println("Error when starting transaction");
-        }
-    }
-    void rollback() {
-        var statement = "ROLLBACK";
-        try (var conn = DatabaseManager.getConnection()) {
-            var startStatement = conn.prepareStatement(statement);
-            startStatement.execute();
-        }
-        catch (Exception e) {
-            System.out.println("Rollback check database integrity");
+            var query = conn.prepareStatement("SELECT COUNT(*) AS rowCount FROM users");
+            var response = query.executeQuery();
+            if (response.next()) {
+                return response.getInt(1);
+            }
+            else {
+                return 0;
+            }
+        } catch (Exception e) {
+            fail(e.getMessage());
+            return -1;
         }
     }
 
