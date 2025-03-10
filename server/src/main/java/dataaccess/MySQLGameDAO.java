37881690dec7bc78a3892c1de91fdc309bbf5e01
guymt61 <jacob.leetham@gmail.com>
2025-03-09 22:38:40 -0600

    First attempt at clear, createGame, getGame, and updateGame methods for MySQLGameDAO

--- server/src/main/java/dataaccess/MySQLGameDAO.java
+++ server/src/main/java/dataaccess/MySQLGameDAO.java
@@ -1,10 +1,14 @@
 package dataaccess;
 
+import com.google.gson.Gson;
 import exception.ResponseException;
 import model.GameData;
 
 import java.sql.*;
 import java.util.HashSet;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class MySQLGameDAO implements GameDAO {
 
@@ -13,18 +17,59 @@ public class MySQLGameDAO implements GameDAO {
     }
 
     //Clear all games
-    public void clear() {}
+    public void clear() {
+        var statement = "TRUNCATE games";
+        try {
+            executeUpdate(statement);
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     //Add a new game
-    public void createGame(GameData game) {}
+    public void createGame(GameData game) {
+        var statement = "INSERT INTO games (id, whiteUsername, blackUsername, name, game) VALUES (?, ?, ?, ?, ?)";
+        var json = new Gson().toJson(game.game());
+        try {
+            executeUpdate(statement, game.gameID(), game.whiteUsername(), game.blackUsername(), game.gameName(), json);
+        }
+        catch (Throwable ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
     //Find a game based on ID
     public GameData getGame(int id) {
+        var statement = "SELECT game FROM games WHERE id=?";
+        try (var conn = DatabaseManager.getConnection()) {
+            var ps = conn.prepareStatement(statement);
+            ps.setInt(1, id);
+            var rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Gson().fromJson(rs.getString(1), GameData.class);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
     //Update the chess game at a specified ID
-    public void updateGame(GameData game) throws DataAccessException {}
+    public void updateGame(GameData game) throws DataAccessException {
+        if (getGame(game.gameID()) == null) {
+            throw new DataAccessException("Cannot update game that doesn't exist");
+        }
+        var statement = "UPDATE games SET game=? WHERE id=?";
+        try {
+            var json = new Gson().toJson(game.game());
+            executeUpdate(statement, json, game.gameID());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     //Produce a list of all GameData
     public HashSet<GameData> listGames() {
@@ -34,12 +79,13 @@ public class MySQLGameDAO implements GameDAO {
     private final String[] createStatements = {
             """
 CREATE TABLE IF NOT EXISTS games (
-'id' int NOT NULL
-'whiteUsername' varchar(256)
-'blackUsername' varchar(256)
-'name' varchar(256) NOT NULL
-'game' text NOT NULL
-PRIMARY KEY ('id')
+`id` int NOT NULL,
+`whiteUsername` varchar(256),
+`blackUsername` varchar(256),
+`name` varchar(256) NOT NULL,
+`game` text NOT NULL,
+PRIMARY KEY (`id`),
+INDEX(id)
 )
 """
     };
@@ -54,6 +100,29 @@ PRIMARY KEY ('id')
             }
         } catch (SQLException ex) {
             throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+
+    private int executeUpdate(String statement, Object... params) throws ResponseException, DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof Integer integer) ps.setInt(i + 1, integer);
+                    else if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
         }
     }
 
