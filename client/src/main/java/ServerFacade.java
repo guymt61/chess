53332ef28bc3bf4b2e4cc42edfa3ab1c22f9b65b
guymt61 <jacob.leetham@gmail.com>
2025-03-13 16:00:13 -0600

    Incomplete first pass at ServerFacade. Should probably be refactored to properly implement request and response objects.

---
+++ client/src/main/java/ServerFacade.java
@@ -0,0 +1,119 @@
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.*;
+
+import java.io.*;
+import java.net.*;
+
+public class ServerFacade {
+
+    private final String serverUrl;
+
+    public ServerFacade(String url) {
+        serverUrl = url;
+    }
+
+
+    public AuthData register(String username, String password, String email) throws ResponseException {
+        var path = "/user";
+        UserData user = new UserData(username, password, email);
+        return this.makeRequest("POST", path, user, null, AuthData.class);
+    }
+
+    public AuthData login(String username, String password) throws ResponseException {
+        var path = "/session";
+        UserData user = new UserData(username, password, null);
+        return this.makeRequest("POST",path, user, null, AuthData.class);
+    }
+
+    public void logout(String authToken) throws ResponseException {
+        var path = "/session";
+        this.makeRequest("DELETE", path, null, authToken, null);
+    }
+
+    public void clear() throws ResponseException {
+        var path = "/db";
+        this.makeRequest("DELETE", path, null, null, null);
+    }
+
+    public GameData[] list(AuthData auth) throws ResponseException {
+        String authToken = auth.authToken();
+        var path = "/game";
+        record listGameResponse(GameData[] game) {
+        }
+        var response = this.makeRequest("GET", path, null, authToken, listGameResponse.class);
+        return response.game();
+    }
+
+    public int create(String gameName, String authToken) throws ResponseException{
+        var path = "/game";
+        return this.makeRequest("POST", path, gameName, authToken, Integer.class);
+    }
+
+    public void join(String playerColor) throws ResponseException {
+        var path = "/game";
+    }
+
+    private <T> T makeRequest(String method, String path, Object request, String authToken, Class<T> responseClass) throws ResponseException {
+        try {
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            http.setRequestMethod(method);
+            http.setDoOutput(true);
+
+            writeBody(request, http);
+            if (authToken != null) {
+                http.addRequestProperty("Authorization", authToken);
+            }
+            http.connect();
+            throwIfNotSuccessful(http);
+            return readBody(http, responseClass);
+        } catch (ResponseException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    private static void writeBody(Object request, HttpURLConnection http) throws IOException {
+        if (request != null) {
+            http.addRequestProperty("Content-Type", "application/json");
+            String reqData = new Gson().toJson(request);
+            try (OutputStream reqBody = http.getOutputStream()) {
+                reqBody.write(reqData.getBytes());
+            }
+        }
+    }
+
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw ResponseException.fromJson(respErr);
+                }
+            }
+
+            throw new ResponseException(status, "other failure: " + status);
+        }
+    }
+
+    private static <T> T readBody(HttpURLConnection http, Class<T> responseClass) throws IOException {
+        T response = null;
+        if (http.getContentLength() < 0) {
+            try (InputStream respBody = http.getInputStream()) {
+                InputStreamReader reader = new InputStreamReader(respBody);
+                if (responseClass != null) {
+                    response = new Gson().fromJson(reader, responseClass);
+                }
+            }
+        }
+        return response;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+}
