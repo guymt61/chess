19f71c316982c06e5da339cf798e738bc6ce7f49
guymt61 <jacob.leetham@gmail.com>
2025-03-18 13:31:46 -0600

    Implementation of ServerFacade unit tests for register method

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,16 +1,33 @@
+import client.ServerFacade;
+import exception.ResponseException;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    private static ServerFacade facade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String url = String.format("http://localhost:%s",port);
+        facade = new ServerFacade(url);
+    }
+
+    @BeforeEach
+    public void clearData() {
+        try {
+            facade.clear();
+        } catch (ResponseException e) {
+            System.out.println("Could not clear database: " + e.getMessage());
+        }
     }
 
     @AfterAll
@@ -20,8 +37,22 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @DisplayName("Good register request")
+    public void goodRegister() throws ResponseException{
+            AuthData authData = facade.register("testUser1", "SuperSecure", "test@test.test");
+            assertNotNull(authData);
+            assertTrue(authData.authToken().length() > 10);
+    }
+
+    @Test
+    @DisplayName("Bad register request")
+    public void badRegister(){
+        try {
+            AuthData authData = facade.register("NoPassword", null, "test@test.test");
+            fail("Register should have thrown an error for null password");
+        } catch (ResponseException e) {
+            //yippee
+        }
     }
 
 }
