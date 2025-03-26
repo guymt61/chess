4d4b91cadb167ed73b02bddfb4c225da410f1675
guymt61 <jacob.leetham@gmail.com>
2025-03-26 14:11:32 -0600

    Preparing get and set methods for different ServerMessage types with warnings when applied to unexpected types

--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -1,16 +1,19 @@
 package websocket.messages;
 
+import chess.ChessGame;
+
 import java.util.Objects;
 
 /**
  * Represents a Message the server can send through a WebSocket
- * 
  * Note: You can add to this class, but you should not alter the existing
  * methods.
  */
 public class ServerMessage {
     ServerMessageType serverMessageType;
     private String message;
+    private String errorMessage;
+    private ChessGame game;
 
     public enum ServerMessageType {
         LOAD_GAME,
@@ -27,17 +30,51 @@ public class ServerMessage {
     }
 
     public void setMessage(String message) {
-        if (serverMessageType == ServerMessageType.LOAD_GAME) {
-            System.out.println("WARNING: unexpected message added to a LOAD_GAME notification");
+        if (serverMessageType != ServerMessageType.NOTIFICATION) {
+            String warningMessage = String.format("WARNING: unexpected message added to a %s ServerMessage", serverMessageType);
+            System.out.println();
         }
         this.message = message;
     }
 
     public String getMessage() {
-        if (serverMessageType == ServerMessageType.LOAD_GAME) {
-            System.out.println("WARNING: unexpected message retrieved from a LOAD_GAME notification");
+        if (serverMessageType != ServerMessageType.NOTIFICATION) {
+            String warningMessage = String.format("WARNING: unexpected message retrieved from a %s ServerMessage", serverMessageType);
+            System.out.println(warningMessage);
         }
         return message;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        if (serverMessageType != ServerMessageType.ERROR) {
+            String warningMessage = String.format("WARNING: Unexpected error message added to %s ServerMessage", serverMessageType);
+            System.out.println(warningMessage);
+        }
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        if (serverMessageType != ServerMessageType.ERROR) {
+            String warningMessage = String.format("WARNING: Unexpected error message retrieved from %s ServerMessage", serverMessageType);
+            System.out.println(warningMessage);
+        }
+        return errorMessage;
+    }
+
+    public void setGame(ChessGame game) {
+        if (serverMessageType != ServerMessageType.LOAD_GAME) {
+            String warningMessage = String.format("WARNING: Unexpected game added to %s ServerMessage", serverMessageType);
+            System.out.println(warningMessage);
+        }
+        this.game = game;
+    }
+
+    public ChessGame getGame() {
+        if (serverMessageType != ServerMessageType.LOAD_GAME) {
+            String warningMessage = String.format("WARNING: Unexpected game retrieved from %s ServerMessage", serverMessageType);
+            System.out.println(warningMessage);
+        }
+        return game;
     }
 
     @Override
